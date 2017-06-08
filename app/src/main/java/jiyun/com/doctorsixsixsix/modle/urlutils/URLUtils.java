41aa795121d40b3902944bb9b36853182963f547
zhangchao1215 <670290530@qq.com>
2017-06-08 22:34:03 +0800

    新添加的baseURL

---
+++ app/src/main/java/jiyun/com/doctorsixsixsix/modle/urlutils/URLUtils.java
@@ -0,0 +1,10 @@
+package jiyun.com.doctorsixsixsix.modle.urlutils;
+
+/**
+ * Created by Administrator on 2017/6/8.
+ */
+
+public class URLUtils {
+    public static final String BASEURL = "http://api.wws.xywy.com/";
+
+}
