9ebf031d80a19acc55500506dc1b67bb5e326944
zhangchao1215 <670290530@qq.com>
2017-06-08 21:03:48 +0800

    在main页面里面写的注释

--- app/src/main/java/jiyun/com/doctorsixsixsix/MainActivity.java
+++ app/src/main/java/jiyun/com/doctorsixsixsix/MainActivity.java
@@ -9,5 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //张超写的，这是我的Main页面
     }
 }
