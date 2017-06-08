114b53aee3ef82ac82cd79cc1df9fdd23f9eacc9
zhangchao1215 <670290530@qq.com>
2017-06-08 21:11:14 +0800

    新建了一个OneActivity

---
+++ app/src/main/java/jiyun/com/doctorsixsixsix/OneActivity.java
@@ -0,0 +1,17 @@
+package jiyun.com.doctorsixsixsix;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Administrator on 2017/6/8.
+ */
+
+public class OneActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
