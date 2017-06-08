c816196ed8fe3451411ba994fc057af6b24d1e49
zhangchao1215 <670290530@qq.com>
2017-06-08 22:01:23 +0800

    新建的项目包

---
+++ app/src/main/java/jiyun/com/doctorsixsixsix/modle/callback/MyCallBack.java
@@ -0,0 +1,13 @@
+package jiyun.com.doctorsixsixsix.modle.callback;
+
+/**
+ * Created by Administrator on 2017/6/8.
+ */
+
+public interface MyCallBack {
+
+    void onSuccess(String GsonData);
+
+    void onError(String errorMsg);
+
+}
