b632269d4ab7511b642dbbbdb5c7ce2ed1f10aa9
wangben0122 <1124979627@qq.com>
2017-06-08 20:00:43 +0800

    wangben

---
+++ app/src/main/java/jiyun/com/doctorsixsixsix/WangBen.java
@@ -0,0 +1,16 @@
+package jiyun.com.doctorsixsixsix;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by admin on 2017/6/8.
+ */
+
+public class WangBen extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
