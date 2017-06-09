df096943277e794f7318b2fad44cd78177658ecd
dell <https://git.oschina.net/chunjunQAQ/myapp.git>
2017-06-09 11:50:37 +0800

    layout

---
+++ app/src/main/java/jiyun/com/doctorsixsixsix/view/MainActivity.java
@@ -0,0 +1,30 @@
+package jiyun.com.doctorsixsixsix.view;
+
+import jiyun.com.doctorsixsixsix.R;
+import jiyun.com.doctorsixsixsix.base.BaseActivity;
+
+/**
+ * Created by dell on 2017/6/9.
+ */
+
+public class MainActivity extends BaseActivity {
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initListener() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
