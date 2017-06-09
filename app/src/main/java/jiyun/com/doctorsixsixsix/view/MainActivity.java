50c4186aa877452e55f719ad839a994660a64fd0
dell <https://git.oschina.net/chunjunQAQ/myapp.git>
2017-06-09 12:01:17 +0800

    layout

--- app/src/main/java/jiyun/com/doctorsixsixsix/view/MainActivity.java
+++ app/src/main/java/jiyun/com/doctorsixsixsix/view/MainActivity.java
@@ -1,5 +1,11 @@
 package jiyun.com.doctorsixsixsix.view;
 
+import android.graphics.drawable.Drawable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.widget.TextView;
+
 import jiyun.com.doctorsixsixsix.R;
 import jiyun.com.doctorsixsixsix.base.BaseActivity;
 
@@ -8,6 +14,8 @@ import jiyun.com.doctorsixsixsix.base.BaseActivity;
  */
 
 public class MainActivity extends BaseActivity {
+    private ViewPager mViewPager;
+    private TabLayout mTabLayout;
     @Override
     protected int getLayoutId() {
         return R.layout.activity_main;
@@ -15,6 +23,8 @@ public class MainActivity extends BaseActivity {
 
     @Override
     protected void initView() {
+        mViewPager= (ViewPager) findViewById(R.id.Main_ViewPager);
+        mTabLayout= (TabLayout) findViewById(R.id.Main_Tab);
 
     }
 
@@ -26,5 +36,13 @@ public class MainActivity extends BaseActivity {
     @Override
     protected void initData() {
 
+    }
+    private TextView getText(String str,int drawableID){
+        TextView textView= (TextView) LayoutInflater.from(this).inflate(R.layout.text_item,null).findViewById(R.id.Main_Text);
+        Drawable drawable=getResources().getDrawable(drawableID);
+        drawable.setBounds(0, 0, drawable.getMinimumWidth(), drawable.getMinimumHeight());
+        textView.setCompoundDrawables(null, drawable, null, null);
+        textView.setText(str);
+        return textView;
     }
 }
---
+++ app/src/main/java/jiyun/com/doctorsixsixsix/view/fragment/TestFragment.java
@@ -0,0 +1,29 @@
+package jiyun.com.doctorsixsixsix.view.fragment;
+
+import jiyun.com.doctorsixsixsix.base.BaseFragment;
+
+/**
+ * Created by dell on 2017/6/9.
+ */
+
+public class TestFragment extends BaseFragment {
+    @Override
+    protected int getLayoutId() {
+        return 0;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initListener() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
