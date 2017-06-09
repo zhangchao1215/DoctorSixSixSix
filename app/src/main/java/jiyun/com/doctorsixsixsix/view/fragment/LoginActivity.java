ff50e480d42591a3aac7bd74ee0587e53bb59987
wangben0122 <1124979627@qq.com>
2017-06-09 20:28:54 +0800

    wangben

---
+++ app/src/main/java/jiyun/com/doctorsixsixsix/view/fragment/LoginActivity.java
@@ -0,0 +1,34 @@
+package jiyun.com.doctorsixsixsix.view.fragment;
+
+import jiyun.com.doctorsixsixsix.base.BaseActivity;
+
+/**
+ * 项目名称: 血压卫士
+ * 类描述:
+ * 创建人: admin
+ * 创建时间: 2017/6/9 19:56
+ * 修改人:
+ * 修改内容:
+ * 修改时间:
+ */
+public class LoginActivity extends BaseActivity {
+    @Override
+    protected int getLayoutId() {
+        return 0;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initListener() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
