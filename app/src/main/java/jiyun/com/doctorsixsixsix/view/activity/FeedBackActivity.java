938597e3a2c38e8dafed576b21e43b96e2bb5b8f
dell <https://git.oschina.net/chunjunQAQ/myapp.git>
2017-06-13 09:31:25 +0800

    Test

---
+++ app/src/main/java/jiyun/com/doctorsixsixsix/view/activity/FeedBackActivity.java
@@ -0,0 +1,36 @@
+package jiyun.com.doctorsixsixsix.view.activity;
+
+import jiyun.com.doctorsixsixsix.R;
+import jiyun.com.doctorsixsixsix.base.BaseActivity;
+
+/**
+ * 项目名称: 血压卫士
+ * 类描述:
+ * 创建人: dell
+ * 创建时间: 2017/6/13 9:22
+ * 修改人:
+ * 修改内容:
+ * 修改时间:
+ */
+
+public class FeedBackActivity extends BaseActivity {
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_feedback;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initListener() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
