d59b2ee25a67fd7d577c61bd69c82730baf393b7
zhangchao1215 <670290530@qq.com>
2017-06-15 15:21:42 +0800

    改动的提问医生的布局

--- app/src/main/java/jiyun/com/doctorsixsixsix/view/fragment/DoctorFragment.java
+++ app/src/main/java/jiyun/com/doctorsixsixsix/view/fragment/DoctorFragment.java
@@ -5,12 +5,9 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
-import android.os.Bundle;
 import android.os.Handler;
 import android.view.Gravity;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.GridView;
@@ -26,7 +23,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
-import butterknife.ButterKnife;
 import butterknife.OnClick;
 import butterknife.Unbinder;
 import jiyun.com.doctorsixsixsix.App;
@@ -403,6 +399,7 @@ public class DoctorFragment extends BaseFragment implements MainDoctorView {
 
     private Handler handler = new Handler();
 
+
     private void myMapDialog() {
         new AlertDialog.Builder(getActivity()).setTitle("温馨提示").setMessage("您是否允许健康血压使用定位功能.").setNegativeButton("取消", new DialogInterface.OnClickListener() {
             @Override
@@ -427,17 +424,5 @@ public class DoctorFragment extends BaseFragment implements MainDoctorView {
 
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // TODO: inflate a fragment view
-        View rootView = super.onCreateView(inflater, container, savedInstanceState);
-        unbinder = ButterKnife.bind(this, rootView);
-        return rootView;
-    }
 
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        unbinder.unbind();
-    }
 }
